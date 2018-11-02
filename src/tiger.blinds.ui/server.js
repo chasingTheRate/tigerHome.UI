40123c240ef545228028c621907dc001a8db2f70
Mark <mark.e.eaton@gmail.com>
2018-11-02 00:07:11 -0500

    port error?

--- src/tiger.blinds.ui/server.js
+++ src/tiger.blinds.ui/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
-const PORT = config.port;
+const PORT = process.env.PORT || 3000;
 
 console.log(`Env: ${ config.env }`);
 
