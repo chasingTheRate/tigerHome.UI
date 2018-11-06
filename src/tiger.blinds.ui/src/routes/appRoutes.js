ee39a2e20301cb3666fd4d25ceac84129bdeb833
Mark <mark.e.eaton@gmail.com>
2018-11-06 07:54:32 -0600

    test

--- src/tiger.blinds.ui/src/routes/appRoutes.js
+++ src/tiger.blinds.ui/src/routes/appRoutes.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
-import DashboardPage from '../pages/dashboardPage/dashboardPage';
+import DashboardPage2 from '../pages/dashboardPage/dashboardPage';
 
 class AppRoutes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path='/' component={ DashboardPage }/>
+          <Route exact path='/' component={ DashboardPage2 }/>
         </Switch>
       </div>
     );
