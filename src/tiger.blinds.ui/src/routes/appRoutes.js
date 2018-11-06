9a07ba08a9d1abfee3459aaddb9ba56bf0d39d6b
Mark <mark.e.eaton@gmail.com>
2018-11-06 07:22:43 -0600

    dashboardPage may be corrupt

--- src/tiger.blinds.ui/src/routes/appRoutes.js
+++ src/tiger.blinds.ui/src/routes/appRoutes.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
+import DashboardPage from '../pages/dashboardPage/dashboardPage';
 
 class AppRoutes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path='/'/>
+          <Route exact path='/' component={ DashboardPage }/>
         </Switch>
       </div>
     );
