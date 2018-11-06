cac3b9fcbdc1c9306b2d038d3b9cd32dbf00149c
Mark <mark.e.eaton@gmail.com>
2018-11-06 07:19:21 -0600

    testing corrupt file?

--- src/tiger.blinds.ui/src/routes/appRoutes.js
+++ src/tiger.blinds.ui/src/routes/appRoutes.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
-import DashboardPage from '../pages/dashboardPage/dashboardPage';
 
 class AppRoutes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path='/' component={ DashboardPage }/>
+          <Route exact path='/'/>
         </Switch>
       </div>
     );
