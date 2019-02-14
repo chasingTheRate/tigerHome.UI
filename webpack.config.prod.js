28a1a6c5da4bae9791ad2f0162372e9f0345c948
markeaton <markeaton@Marks-Mac-mini.local>
2019-02-14 17:25:50 -0600

    test

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,8 +4,6 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.common');
 
-console.log(`test: ${process.env.TIGER_BLINDS_API}`);
-
 module.exports = merge( common, {
   mode: 'production',
   plugins: [
@@ -15,9 +13,11 @@ module.exports = merge( common, {
       template: "./src/templates/index.html",
       filename: "./index.html"
     }),
-    new webpack.DefinePlugin({           
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV),      
-      TIGER_BLINDS_API: JSON.stringify(process.env.TIGER_BLINDS_API)
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        TIGER_BLINDS_API: JSON.stringify(process.env.TIGER_BLINDS_API)
+      }      
     })
   ]
 });
