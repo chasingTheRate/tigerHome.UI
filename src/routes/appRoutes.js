c20da7f649fdf86992fdc240d50f48212266acec
Mark <mark.e.eaton@gmail.com>
2019-07-25 19:19:10 -0500

    fixed saving bug

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -3,11 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/homePage/';
 import DashboardPage from '../pages/dashboardPage/dashboardPage';
 import BlindPage from '../pages/blindPage/blindPage';
-<<<<<<< HEAD
-import Home from '../pages/home';
-=======
 import AccessoryRoutes from './accessoryRoutes';
->>>>>>> change-header-title
 
 class AppRoutes extends Component {
   render() {
@@ -16,12 +12,8 @@ class AppRoutes extends Component {
         <Switch>
           <Route exact path='/blinds/:id' component={ BlindPage }/>
           <Route exact path='/blinds' component={ DashboardPage }/>
-<<<<<<< HEAD
-          <Route exact path='/' component={ Home }/>
-=======
           <Route path='/accessory' component={ AccessoryRoutes }/>
           <Route exact path='/' component={ HomePage }/>
->>>>>>> change-header-title
         </Switch>
       </div>
     );
